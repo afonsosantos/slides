5477ab9feb6faf73a95b72960751142a493a78b7
Afonso Santos <afonso.faisca1@gmail.com>
2026-08-26 22:12:13 +0100

    Small spacing fix

--- pycon-pt-2026/global-bottom.vue
+++ pycon-pt-2026/global-bottom.vue
@@ -15,7 +15,7 @@ const { currentLayout, currentPage, total } = useNav()
     v-if="!['cover', 'end'].includes(currentLayout)"
     class="absolute bottom-10 left-14 z-10 flex items-center gap-6 pointer-events-none"
   >
-    <img src="/pycon-logo.svg" class="h-7 object-contain" alt="PyCon Portugal 2026"/>
+    <img src="/pycon-logo.svg" class="h-7 object-contain" alt="PyCon Portugal 2026" />
     <div class="logo-sep"></div>
     <img src="/visionsoft-white.svg" class="h-6 object-contain" alt="Visionsoft" />
   </div>
